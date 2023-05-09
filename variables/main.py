5e8bdb49bbb8f4101f9587bd659e639a217e8ae1
ccaccachahuafeli <ccaccachahuafeliciana@gmail.com>
2023-05-09 10:22:42 -0500

    primera clase de pythion

---
+++ variables/main.py
@@ -0,0 +1,31 @@
+## es un espacio en memoria donde se podemos almacenar datos 
+nombre="edwin"
+##nombre = -> es el identificador del 
+## espacio de memoria que vamos a usar .
+## = -> es el signo de asignación que nos indica que vamos a almacenar
+## edwin -> es el dato que estamos almacenando.
+## el identificador _(nombre) como apunta a un espacio de memmoria
+## no se puede cambiar o editar.
+## sin embargo el dato que almacena puede mutar en cualquier momento.
+## pasos para declarar una variable:
+## 1. darle un nombre al identificadior (variable)
+## 2. escribir el operador de asignación (=)
+## 3. escribir el dato  a almacenar
+
+## reglas para nombrar variables
+## 1. solo puede tener los siguientes caracteres
+## -letras minusculas
+## - letras mayusculas
+## - digitos 
+## - guiones bajos(_)
+## 2. solo deebn iniciar con letras y guion bajo nocon digitos.
+## 3. no puede ser una palabra reservada del lenguaje.
+
+## ejemplos de variables validas
+a='valido'              -> 3='invalido'
+a3='valido'             ->3a='invalido'
+a_b_c='valido'          ->mi-variable='invalido'
+_abc='valido'           ->with='ivalido'
+_3a='valido'            ->3_a='invalido'
+
+
